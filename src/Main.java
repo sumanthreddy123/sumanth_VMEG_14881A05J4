6c2507d7c87a7efad888f295b97074a5ce4847c5
sumanthreddy123 <31913499+sumanthreddy123@users.noreply.github.com>
2017-09-13 14:12:24 +0530

    before

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,12 @@ public class Main {
 		//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before to submit
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		Date e = new Date();
+
+		Student[] stu = new Student[n];
+		
 	}
 
-}
+}s
